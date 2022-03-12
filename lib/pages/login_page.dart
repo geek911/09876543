3175bacf879d16a15a770b8ba9abecf5c6b62660
Moses Chawawa <moseschawawa@gmail.com>
2022-03-12 17:07:05 +0200

    added login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _login() {
+    return true;
+  }
+
+  void _register(BuildContext context) async {
+    await Navigator.of(context).pushNamed('/register');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      // appBar: AppBar(),
+      body: Container(
+          margin: const EdgeInsets.only(left: 20, right: 20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.mail),
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 5))),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.key),
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 5))),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton.icon(
+                    label: const Text(
+                      "Login",
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    icon: const Icon(Icons.lock),
+                    onPressed: _login,
+                  ),
+                ),
+                TextButton(
+                  child: const Text('Not yeat a user? Register here'),
+                  onPressed: () => _register(context),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
