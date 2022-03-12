d3ddf117607ead6c15396ba85e7071b2da05ed21
Moses Chawawa <moseschawawa@gmail.com>
2022-03-12 16:29:55 +0200

    Added name routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:food_donor/pages/login_page.dart';
+import 'package:food_donor/pages/register_page.dart';
+import 'package:food_donor/pages/profile_page.dart';
+import 'package:food_donor/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/profile': (context) => ProfilePage(),
+        '/home': (context) => HomePage(),
+      },
+      initialRoute: '/login',
+    );
   }
 }
