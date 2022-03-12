193d1aacaffd9a81ed41b6e684942c3767459545
Moses Chawawa <moseschawawa@gmail.com>
2022-03-12 16:19:05 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/food_donor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_donor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:food_donor/app.dart';
+
+void main() => runApp(App());
